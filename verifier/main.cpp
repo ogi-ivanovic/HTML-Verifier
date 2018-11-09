8ce6cd0183d60eed652a0bbe2c0bf202a7c2565d
Ogi Ivanovic <ogiivano99@gmail.com>
2018-11-09 14:22:57 -0500

    Add files via upload

---
+++ verifier/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <sstream>
+#include <fstream>
+#include "verification.h"
+
+int main(int argc, char *argv[]) {
+    std::string file = argv[1]; // opens the given text file containing html
+    Verification content;
+    content.createStack(file); // create a stack containing all the tags in file
+    content.verify(); // verifies the contents of the file
+}
